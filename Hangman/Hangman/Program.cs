4fa2e6dd6b273b9cac9db5d1ef8cbfd84ea0dadc
Suzanne <suzannemaggioni@gmail.com>
2023-04-03 18:00:27 -0400

    First ideas for word bank and initial user prompt.

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace Hangman;
 class Program
 {
+    const int STARTING_GUESS_COUNT = 10;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string availableLetters = ""; //make an array of letters to choose from
+        string lettersAlreadyUsed = ""; //make an array of letters already used
+        string fiveLetterWord = "_ _ _ _ _";
+
+        List<string> wordBank = new List<string>();
+        {
+            wordBank.Add("think");
+            wordBank.Add("flame");
+            wordBank.Add("train");
+            wordBank.Add("clean");
+            wordBank.Add("great");
+        }
+
+        Console.WriteLine("Let's play hangman!");
+        Console.WriteLine(fiveLetterWord);
+        Console.Write($"Guess a letter. You have made # incorrect guesses. Only # wrong guesses remaining."); //Needs variables to track number of guesses left.
+        Console.WriteLine($"You have already used these letters: {lettersAlreadyUsed}");
+        string userGuess = Console.ReadLine();
     }
 }
 
