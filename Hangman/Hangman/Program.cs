70af023a5cbe5a43a1a92c7c745263fed113355c
Suzanne <suzannemaggioni@gmail.com>
2023-04-06 19:31:48 -0400

    For each loop work. Attempts to match user guess with letters in the word.

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -5,29 +5,33 @@ class Program
 
     static void Main(string[] args)
     {
-        string blankWord = "_ _ _ _ _";
-        string lettersAlreadyUsed = ""; //array of letters already used
+        string[] blankWordArray = { "_____" };
+        string lettersAlreadyUsed = ""; //string of letters already used
 
-        
-        List<string> wordBank = new List<string>() {"think", "flame", "train", "clean", "great"};
+        var wordBank = new List<string>() { "think", "flame", "train", "clean", "great" };
 
         //Random word from list - Will need to go in a loop
         Random rng = new Random();
         int randomListItem = rng.Next(wordBank.Count);
 
         Console.WriteLine("Let's play hangman!");
-        Console.WriteLine(blankWord);
+        Console.WriteLine(blankWordArray);
         Console.Write($"Guess a letter. You have made # incorrect guesses. Only # wrong guesses remaining."); //Needs variables to track number of guesses left.
         Console.WriteLine($"You have already used these letters: {lettersAlreadyUsed}");
-        string userGuess = Console.ReadLine();
+        string userGuess = Console.ReadLine().ToLower();
+        char userGuessConvertedToChar = Convert.ToChar(userGuess);
 
-        Console.WriteLine(wordBank[randomListItem]);
-
-        //Use a function to help determine if character is part of the string
-        //foreach (string item in randomListItem)
-        //{
-
-        //}
+        //Determine if character is part of the string
+       
+        foreach (char letter in wordBank[randomListItem])
+        {
+            if (userGuessConvertedToChar == letter)
+            {
+                //How can I specify the location to write
+                blankWordArray[letter] = userGuess;
+                Console.WriteLine(blankWordArray);
+            }
+        }
         
     }
 }
