108515693a3686320d91e1f575e0252ee85a1115
Suzanne <suzannemaggioni@gmail.com>
2023-04-07 19:35:19 -0400

    Set up lists with multiple lengths, considered ways to look through 5 letter words and find locations.

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -5,34 +5,53 @@ class Program
 
     static void Main(string[] args)
     {
-        string[] blankWordArray = { "_____" };
-        string lettersAlreadyUsed = ""; //string of letters already used
-
-        var wordBank = new List<string>() { "think", "flame", "train", "clean", "great" };
-
-        //Random word from list - Will need to go in a loop
         Random rng = new Random();
-        int randomListItem = rng.Next(wordBank.Count);
+        char[] blankWordArray = { '_', ' ' }; //maybe use += in a for loop to repeat for varied word lengths
+        char lettersAlreadyUsed = ' '; //come back to this
 
-        Console.WriteLine("Let's play hangman!");
-        Console.WriteLine(blankWordArray);
-        Console.Write($"Guess a letter. You have made # incorrect guesses. Only # wrong guesses remaining."); //Needs variables to track number of guesses left.
-        Console.WriteLine($"You have already used these letters: {lettersAlreadyUsed}");
-        string userGuess = Console.ReadLine().ToLower();
-        char userGuessConvertedToChar = Convert.ToChar(userGuess);
+        List<string> wordBankFiveLetters = new List<string>() { "think", "flame", "train", "clean", "great" };
+        List<string> wordBankSixLetters = new List<string>();
+        { };
+        List<string> wordBankSevenLetters = new List<string>();
+        { };
 
-        //Determine if character is part of the string
-       
-        foreach (char letter in wordBank[randomListItem])
+        Console.WriteLine("Let's play hangman! Would you like to guess a 5, 6, or 7 letter word?");
+        int wordLength = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < wordLength; i++)
         {
-            if (userGuessConvertedToChar == letter)
-            {
-                //How can I specify the location to write
-                blankWordArray[letter] = userGuess;
-                Console.WriteLine(blankWordArray);
-            }
+            blankWordArray +=;
+            Console.WriteLine(blankWordArray);
         }
-        
+        //Console.WriteLine(blankWordArray[wordLength]); //this doesn't work if using loop to generate blanks
+        //Console.Write($"Guess a letter. You have made # incorrect guesses. Only # wrong guesses remaining."); //Needs variables to track number of guesses left.
+        //Console.WriteLine($"You have already used these letters: {lettersAlreadyUsed}");
+        //string userInput = Console.ReadLine().ToLower();
+        //char guessedLetter = Convert.ToChar(userInput);
+
+        //if (wordLength == 5)
+        //{
+        //    int randomListItem = rng.Next(wordBankFiveLetters.Count);
+        //    foreach (char letter in wordBankFiveLetters[randomListItem])
+        //    {
+        //        for (int i = 0; i < blankWordArray.Length; i++)
+        //        {
+        //            if (wordBankFiveLetters[randomListItem][letter] = guessedLetter)
+        //            //Would this be an instance to use a jagged array?
+        //            {
+        //                blankWordArray[i] = letter;
+        //            }
+        //        }
+        //    }
+        //}
+        //if (wordLength == 6)
+        //{
+
+        //    int randomListItem = rng.Next(wordBankSixLetters.Count);
+        //}
+        //else
+        //{
+
+        //    int randomListItem = rng.Next(wordBankSevenLetters.Count);
+        //}
     }
 }
-
